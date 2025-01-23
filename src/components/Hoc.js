b92f0ed95851a381edbf68a0bb20d3b41d0f306c
Ajay <ajaygrathod5@gmail.com>
2025-01-23 16:50:45 +0530

    Cleanup the code

--- src/components/Hoc.js
+++ src/components/Hoc.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 
 function Hoc(OriginalComponent) {
     
-    return function EnhancedComponent(props) {
+    return function EnhancedComponent() {
         const [count, setCount] = useState(0);
 
         const increment = () => {
             setCount(count + 1);
         };
 
-        return <OriginalComponent count={count} increment={increment} {...props} />;
+        return <OriginalComponent count={count} increment={increment} />;
     };
 }
 
